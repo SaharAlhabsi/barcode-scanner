d2ffce2e80a7025c0de6d578d19d5e1e9969319b
samiio <sami.ali.96@outlook.com>
2021-04-05 14:54:13 +0400

    Unresponsive attempt from documentation

--- app/src/main/java/com/databoat/barcodescanner/ScannerActivity.java
+++ app/src/main/java/com/databoat/barcodescanner/ScannerActivity.java
@@ -11,13 +11,28 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Size;
 import android.view.OrientationEventListener;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class ScannerActivity extends AppCompatActivity {
@@ -44,16 +59,12 @@ public class ScannerActivity extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(this));
     }
 
+    /* https://medium.com/swlh/introduction-to-androids-camerax-with-java-ca384c522c5 */
     private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
         ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder().setTargetResolution(new Size(1280, 720))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
-        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
-            @Override
-            public void analyze(@NonNull ImageProxy image) {
-                image.close();
-            }
-        });
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new BarcodeAnalyzer());
         Preview preview = new Preview.Builder().build();
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
@@ -62,4 +73,55 @@ public class ScannerActivity extends AppCompatActivity {
                 imageAnalysis, preview);
     }
 
+    /*---------------------------------------Barcode Helper---------------------------------------*/
+
+    private class BarcodeAnalyzer implements ImageAnalysis.Analyzer {
+
+        @Override
+        public void analyze(@NonNull ImageProxy imageProxy) {
+            @SuppressLint("UnsafeExperimentalUsageError") Image mediaImage = imageProxy.getImage();
+            if (mediaImage != null) {
+                InputImage image = InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo()
+                        .getRotationDegrees());
+                // Pass image to an ML Kit Vision API
+                BarcodeScannerOptions options = new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(Barcode.FORMAT_CODE_128)
+                        .build();
+                BarcodeScanner scanner = BarcodeScanning.getClient(options);
+
+                // Pass image to the process method
+                Task<List<Barcode>> result = scanner.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                            @Override
+                            public void onSuccess(List<Barcode> barcodes) {
+                                // Task completed successfully
+                                for (Barcode barcode: barcodes) {
+                                    //Rect bounds = barcode.getBoundingBox();
+                                    //Point[] corners = barcode.getCornerPoints();
+
+                                    String rawValue = barcode.getRawValue();
+
+                                    if (barcode.getValueType() == Barcode.FORMAT_CODE_128) {
+                                        String bValue = barcode.getClass().toString();
+                                        Log.i(rawValue, "onSuccess: " + bValue);
+                                    }
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                            }
+                        })
+                        .addOnCompleteListener(new OnCompleteListener<List<Barcode>>() {
+                            @Override
+                            public void onComplete(@NonNull Task<List<Barcode>> task) {
+                                imageProxy.close();
+                            }
+                        });
+            }
+        }
+    }
 }
